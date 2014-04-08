58dac312df5181efba765fcd6e86a5c4092812d2
Jonas Döbertin <hallo@jonasdoebertin.net>
2014-04-08 16:55:01 +0200

    Implemented mailchimp connector

--- classes/SocialBoxConnector.php
+++ classes/SocialBoxConnector.php
@@ -216,8 +216,43 @@ class JD_SocialBoxConnector{
 
     protected static function mailchimp($item) {
 
-        
+        /* Extract data center from api key */
+        $datacenter = 'us1';
+        if(strstr($item['api_key'], '-')) {
+            list($key, $datacenter) = explode('-', $item['api_key'], 2);
+            if(!$datacenter) $datacenter = 'us1';
+        }
 
+        /* Build data object */
+        $data = array(
+            'apikey' => $item['api_key'],
+            'filters' => array(
+                'list_id' => $item['id'],
+            ),
+            'limit' => 1,
+        );
+
+        /* Fetch data from MailChimp API v2.0 */
+        $result = wp_remote_post(sprintf('https://%s.api.mailchimp.com/2.0/lists/list.json', $datacenter), array('sslverify' => false, 'body' => $data));
+
+        /* Check for common errors */
+        if(self::wasCommonError($result)) {
+            return array('successful' => false);
+        }
+
+        /* Decode response */
+        $data = json_decode(wp_remote_retrieve_body($result), true);
+
+        /* Check for incorrect data */
+        if(!is_array($data) or !isset($data['data'][0]['stats']['member_count'])){
+            return array('successful' => false);
+        }
+
+        /* Return value */
+        return array(
+            'successful' => true,
+            'value'      => $data['data'][0]['stats']['member_count'],
+        );
     }
 
 	/**
