162bd3d611ad8102e1417c93f12e31fe9a114843
Jonas Döbertin <hallo@jonasdoebertin.net>
2014-05-13 10:30:03 +0200

    Bumped version numbers to 1.6.3

--- views/options-page/log.php
+++ views/options-page/log.php
@@ -2,7 +2,7 @@
 
 
 /*
- * SocialBox 1.6.2
+ * SocialBox 1.6.3
  * Copyright by Jonas Döbertin
  * Available only at CodeCanyon: http://codecanyon.net/item/socialbox-social-wordpress-widget/627127
  */
